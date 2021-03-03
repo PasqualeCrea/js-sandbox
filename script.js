937b6dcdd03778498b2126b05cbf0c6d21e2ce2c
Pasquale <pasquale.crea@west-mec.org>
2021-03-03 08:46:13 -0700

    Completed telport() function

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ colorSelector.onchange = function () {
     document.getElementById("colorBox").style = `background-color: ${colorSelector.value};`;
 };
 
-function toggleDarkMode(){
+function toggleDarkMode() {
     let content = document.getElementById("content");
     content.classList.toggle("dark-mode");
-    //alert("TOGGLE ME ");
 }
 
-function teleport(){
-    console.log("TELEPort");
+function teleport(distance) {
     let runaway = document.getElementById("runaway");
-    runaway.innerText = "Something else.";
+    console.log(runaway.style.top, " - ", runaway.style.left);
+    if (runaway.style.top == 0 && runaway.style.left == 0) {
+        runaway.style.top = `${distance}px`;
+    } else if (runaway.style.top == `${distance}px` && runaway.style.left == 0) {
+        runaway.style.left = `${distance}px`;
+    } else if (runaway.style.top == `${distance}px` && runaway.style.left == `${distance}px`) {
+        runaway.style.top = 0;
+    } else if (runaway.style.top == "0px" && runaway.style.left == `${distance}px`) {
+        runaway.style.left = null;
+        runaway.style.top = null;
+    }
+    runaway.innerText = "Good Luck!!!";
 }
